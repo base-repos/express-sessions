0a4c1841d826c8d6743776ddc9e247f9e212c31d
Qarun Qadir Bissoondial <qarun.dev@gmail.com>
2021-08-17 00:56:29 -0400

    updated cookie settings

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(session({
     secret: '<INSERT-YOUR-SECRET-HERE>',
+    resave: false,
+    saveUninitialized: false,
     cookie: {
         secure: false // change to true for production environments
     }
@@ -13,13 +15,15 @@ app.use(session({
 
 app.get('/get-visits', (req, res) => {
     addToSession(req.session);
-    res.status(200).send({ success: true, visits: req.session.visits });
+    const { visits } = req.session;
+    res.status(200).send({ success: true, visits });
 });
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
 
+// increment the visits on session if present and sets it to 1 if not present
 function addToSession(session) {
     session.visits = session.visits ? session.visits + 1 : 1;
 }
